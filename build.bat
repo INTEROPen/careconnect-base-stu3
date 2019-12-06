c91e3a338d96b37cdc1c7959cfe2a52eeb100b13
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2019-12-06 11:04:01 +0000

    Profiles changes to fix #3 #4 #5 #6 #7 #8 #9

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
-JAVA -Xms64m -Xmx2g -jar input-cache/org.hl7.fhir.publisher.jar -ig ig.ini -destination docs
-#rmdir /q /s docs
-#rename output docs
+JAVA -Xms64m -Xmx4g -jar input-cache/org.hl7.fhir.publisher.jar -ig ig.ini -destination docs
+rmdir /q /s docs
+del output\*.md
+rename output docs
