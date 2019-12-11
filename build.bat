294dc5802c723c6b78171acd2b9a211a27dc849c
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2019-12-11 17:48:44 +0000

    Missing files from build

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-JAVA -Xms64m -Xmx4g -jar input-cache/org.hl7.fhir.publisher.jar -ig ig.ini -destination docs
+JAVA -Xms64m -Xmx8g -jar input-cache/org.hl7.fhir.publisher.jar -ig ig.ini -destination docs
 rmdir /q /s docs
 del output\*.md
 rename output docs
