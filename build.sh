56e6df72fc2033cd536f02d5f4a04303a39ca8ce
KevinMayfield <mayfield.g.kev@gmail.com>
2019-12-12 07:30:04 +0000

    Develop branch

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-JAVA -jar input-cache/org.hl7.fhir.publisher.jar -ig ig.ini -tool jekyll -destination docs
+JAVA -Xms64m -Xmx4g -jar input-cache/org.hl7.fhir.publisher.jar -ig ig.ini -tool jekyll -destination docs
 rm -r docs
 rm output/*.md
 mv output docs
